9ed5eee6f53c23491b60c1dbb05db964ed8a473e
ElWaje <elwaje@gmail.com>
2023-11-01 20:40:10 +0100

    Ej1 realizado

--- src/ejercicio-01/index.js
+++ src/ejercicio-01/index.js
@@ -1,1 +1,13 @@
-// Escribe tu código aquí:
+let person = {
+    name: "Juan",
+    age: 30,
+    city: "Madrid"
+};
+
+console.log(person.name); 
+console.log(person.age);  
+console.log(person.city); 
+
+console.log(person['name']); 
+console.log(person['age']);  
+console.log(person['city']);
