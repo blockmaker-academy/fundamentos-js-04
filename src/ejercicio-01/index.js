c5b108e7981f0f236f53c8fe1247f46584edfdbe
A05k-Mikhael-luX <anastasiayanastasia.tarot@gmail.com>
2023-11-03 18:11:41 +0100

    Añadir commit Ejercicio-1

--- src/ejercicio-01/index.js
+++ src/ejercicio-01/index.js
@@ -7,7 +7,7 @@ city: 'New York'
 
 console.log(person.name)//notación de puntos
 console.log(person['age'])//notación de corchetes
-console.log(person.city)
-console.log(person['name'])
-console.log(person['city'])
+
+
+
 
