d72a6fd5dbb8bd494fe00a56c9dc38f629aecabe
A05k-Mikhael-luX <anastasiayanastasia.tarot@gmail.com>
2023-11-03 18:18:41 +0100

    Añadir commit Ejercicio-01

--- src/ejercicio-01/index.js
+++ src/ejercicio-01/index.js
@@ -1,1 +1,9 @@
 // Escribe tu código aquí:
+const person = {
+name:'juan',
+age: 12,
+city:'granada'
+}
+console.log(person.name)//acceso con notación de puntos
+console.log(person['age'])//acceso notacón corchetes
+
