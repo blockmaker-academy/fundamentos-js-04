0d2f15e1039513cee038e779a287b7f023bdcaa4
johannymk7 <johannymk7@github.com>
2023-11-06 20:51:17 +0100

    Añadir ejercicio-01

--- src/ejercicio-01/index.js
+++ src/ejercicio-01/index.js
@@ -1,1 +1,9 @@
 // Escribe tu código aquí:
+const person = {
+    name: 'Johon',
+    age: 30,
+    city: 'Barcelona'
+}
+
+console.log('Notacion de puntos',person.name) // Acceso con notación de puntos
+console.log('Notacion de corchetes',person['name']) // Acceso con notacción de corchetes
