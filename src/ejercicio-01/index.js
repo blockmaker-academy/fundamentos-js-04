f1218c49ab2f4692320bcfe0b2a98505c948d262
narcispana <narcisscoot10@gmail.com>
2024-02-07 12:39:06 +0100

    Añadir ejercicio-01

--- src/ejercicio-01/index.js
+++ src/ejercicio-01/index.js
@@ -1,1 +1,9 @@
-// Escribe tu código aquí:
+const person = {
+    name: ' Narcis',
+    age: 25,
+    city: 'Manresa'
+}
+
+console.log(person.name)
+console.log(person['age'])
+console.log(person.city)
